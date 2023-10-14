ca43eda4477f3239d8396fb092486b7a4eb7fece
anabeatriz08 <aninha_luizrolim@hotmail.com>
2023-10-13 22:59:39 -0300

    Sistema de Validação de Processo Seletivo

--- desafio-controle-fluxo/src/Contador.java
+++ desafio-controle-fluxo/src/Contador.java
@@ -5,28 +5,28 @@ public class Contador {
     public static void main(String[] args) {
         Scanner terminal = new Scanner(System.in);
 
-        System.out.println("Digite o primeiro número: ");
+        System.out.println("Digite um número: ");
         int parametroUm = terminal.nextInt();
 
-        System.out.println("Digite o segundo número: ");
+        System.out.println("Digite um outro número: ");
         int parametroDois = terminal.nextInt();
 
         try {
-            contar(parametroUm, parametroDois);
+            contador(parametroUm, parametroDois);
 
         } catch (ParametrosInvalidosException exception) {
             System.out.println("Erro: " + exception);
         }
     }
 
-    static void contar(int parametroUm, int parametroDois) throws ParametrosInvalidosException {
+    static void contador(int parametroUm, int parametroDois) throws ParametrosInvalidosException {
 
         if (parametroUm > parametroDois) {
             throw new ParametrosInvalidosException();
         }
         int contagem = parametroDois - parametroUm;
         for (int i = 1; i <= contagem; i++) {
-            System.out.println("Imprimindo o número " + i);
+            System.out.println("Contagem: " + i);
         }
     }
 }
